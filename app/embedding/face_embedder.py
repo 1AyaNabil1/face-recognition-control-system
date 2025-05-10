805c2fda332a38e32075283c4b27b469daa6b8fc
AyaNabil <ayanabil297@gmail.com>
2025-05-10 21:54:25 +0300

    Integrated InsightFace for robust face embeddings with improved preprocessing.

--- app/embedding/face_embedder.py
+++ app/embedding/face_embedder.py
@@ -11,7 +11,6 @@ class FaceEmbedder:
         self.model.prepare(ctx_id=0)
 
     def get_embedding(self, face):
-        # Resize to InsightFace standard size
         face_resized = cv2.resize(face, (112, 112))
         faces = self.model.get(face_resized)
         if faces and len(faces) > 0:
